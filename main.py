b32de83e641fc9b0fb259384f775511766b4a94c
lan496 <kohei19950508@gmail.com>
2023-03-31 19:19:05 +0900

    Add button to copy output to clipboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Python file to serve as the frontend"""
 import streamlit as st
 from streamlit_chat import message
+import pyperclip
 
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
@@ -20,7 +21,7 @@ def load_chain():
     llm = ChatOpenAI(
         model_name='gpt-3.5-turbo',
         # openai_api_key=None,
-        max_tokens=1024,
+        max_tokens=4096,
         temperature=0,
     )
 
@@ -40,33 +41,45 @@ def load_chain():
     )
     return chain
 
-# From here down is all the StreamLit UI.
-st.set_page_config(page_title="ChatGPT for Poor Man", page_icon=":robot:")
-st.header("ChatGPT for Poor Man")
 
-# Show prompt
-st.info(f"Prompt: {ENGLISH_REVISER_PROMPT}")
+def main():
+    # From here down is all the StreamLit UI.
+    st.set_page_config(page_title="ChatGPT for Poor Man", page_icon=":robot:")
+    st.header("ChatGPT for Poor Man")
 
-# Initialize or reload Chain
-if "chain" not in st.session_state:
-    chain = load_chain()
-    st.session_state['chain'] = chain
+    # Show prompt
+    st.info(f"Prompt: {ENGLISH_REVISER_PROMPT}")
 
-with st.form("chat-form", clear_on_submit=True):
-    user_input = st.text_area("Send a message...")
-    submitted = st.form_submit_button()
+    # Initialize or reload Chain
+    if "chain" not in st.session_state:
+        chain = load_chain()
+        st.session_state['chain'] = chain
 
-    if submitted and (user_input != ""):
-        with st.spinner("Wait for AI..."):
-            st.session_state.chain.predict(input=user_input)
+    # Prepare input
+    with st.form("chat-form", clear_on_submit=True):
+        user_input = st.text_area("Send a message...")
+        submitted = st.form_submit_button()
 
-# Display chat history
-try:
-    messages = st.session_state.chain.memory.chat_memory.messages
-    for i, msg in list(enumerate(messages))[::-1]:
-        if isinstance(msg, HumanMessage):
-            message(msg.content, is_user=True, key=f"{i}_user", avatar_style="thumbs")
-        else:
-            message(msg.content, key=f"{i}")
-except KeyError:
-    pass
+        if submitted and (user_input != ""):
+            with st.spinner("Wait for AI..."):
+                st.session_state.chain.predict(input=user_input)
+
+    # Display chat history
+    try:
+        messages = st.session_state.chain.memory.chat_memory.messages
+        for i, msg in list(enumerate(messages))[::-1]:
+            if isinstance(msg, HumanMessage):
+                message(msg.content, is_user=True, key=f"{i}_user", avatar_style="thumbs")
+            else:
+                col_msg, col_button = st.columns([9, 1])
+                with col_msg:
+                    message(msg.content, key=f"{i}")
+                with col_button:
+                    if st.button('Copy', key=f'{i}_copy'):
+                        pyperclip.copy(msg.content)
+    except KeyError:
+        pass
+
+
+if __name__ == '__main__':
+    main()
